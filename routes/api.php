408cafb8a8bb23a490bb3c0ebf6d21cdcae5a699
mi-shajib <mishajib222@gmail.com>
2026-05-06 19:59:57 +0600

    fix: Reorder API routes to prevent collision with parameterized routes

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,14 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', [ProductController::class, 'getProducts']);
-Route::get('/product/{id}', [ProductController::class, 'getProduct']);
+// Specific routes MUST come before parameterized routes
 Route::post('/product', [ProductController::class, 'addProduct']);
-Route::get('/category/{category}', [ProductController::class, 'getByCategory']);
-Route::get('/filter/topRated', [ProductController::class, 'getTopRated']);
-Route::get('/filter/bestSellers', [ProductController::class, 'getBestSellers']);
+Route::get('/products', [ProductController::class, 'getProducts']);
 Route::get('/products/search', [ProductController::class, 'searchProducts']);
 Route::get('/products/filterBy', [ProductController::class, 'filterProducts']);
 Route::get('/products/{list}', [ProductController::class, 'listOfProducts']);
+
+Route::get('/product/{id}', [ProductController::class, 'getProduct']);
+Route::get('/category/{category}', [ProductController::class, 'getByCategory']);
+Route::get('/filter/topRated', [ProductController::class, 'getTopRated']);
+Route::get('/filter/bestSellers', [ProductController::class, 'getBestSellers']);
